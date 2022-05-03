4758ac4e993cc99fbe8988b0d1698ef6a91bfeb7
Rohan Bhatia <rohanb459@gmail.com>
2022-05-03 12:48:59 +0530

    challenges opened

--- main.js
+++ main.js
@@ -27,4 +27,50 @@ browserOpenPromise.then(function(browser){
 {
     let loginButtonIsPressed = page.click("button[type='submit']");
     return loginButtonIsPressed;
+}).then(function()
+{
+    let clickOnAlgoPromise = waitAndClick("[data-automation='algorithms']", page);
+    return clickOnAlgoPromise;
+}).then(function()
+{
+    let clickOnWarmUpPromise = waitAndClick('input[value="warmup"]', page)
+    return clickOnWarmUpPromise;
+}).then(function()
+{
+    let waitFor3Seconds = page.waitForSelector(".challenge-submit-btn");
+    return waitFor3Seconds;
+}).then(function(){
+    let allChallengesPromise = page.$$(".challenge-submit-btn");
+    return allChallengesPromise;
+}).then(function(questionsArr){
+    console.log("questionArr length is : ", questionsArr.length);
+    questionSolver(questionsArr[0]);
 })
+    
+
+
+function waitAndClick(selector, cPage)
+{
+    return new Promise(function(resolve, reject)
+    {
+        let waitForModelPromise = cPage.waitForSelector(selector);
+        waitForModelPromise.then(function()
+        {
+            let clickModal = cPage.click(selector);
+            return clickModal;
+        }).then(function()
+        {
+            resolve();
+        }).catch(function(err){
+            reject();
+        })
+    })
+}
+
+
+function questionSolver(question){
+    return new Promise(function(resolve, reject){
+        let questionWillBeClicked = question.click();
+        return questionWillBeClicked;
+    })
+}
